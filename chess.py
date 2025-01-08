6164f09b531b794c18696728599cc05f091d5eff
matthew rendely <matthew.rendely@gmail.com>
2025-01-07 20:12:21 -0800

    Add print method to Board

--- chess.py
+++ chess.py
@@ -1,6 +1,6 @@
 class Board:
     def __init__(self):
-        self.squares = [Square(i,j) for i in range(1,9) for j in range(1,9)]
+        self.squares = [Square(j,i) for i in range(1,9) for j in range(1,9)]
         self.setup()
 
     def setup(self):
@@ -16,16 +16,27 @@ class Board:
                     curr_square.connected.append(square)
 
     def place_pieces(self):
-        #pawns
         for square in self.squares:
-            if square.y == 2 or square.y == 7:
-                square.piece = Piece('pawn', 'w' if square.y == 2 else 'b')
+            #white pawns
+            if square.y == 2:
+                square.piece = Piece('Pawn', 'w', '♙')
+
+            #black pawns
+            if square.y == 7:
+                square.piece = Piece('Pawn', 'b', '♟')
     
         #w pawns
 
     def print(self):
-        for square in self.squares:
-            print(square)
+        print(' '+'-'*39)
+        for square in self.squares:            
+            if square.x == 1:
+                print('|',end='')
+            print(f' {square.printable()} |',end='')
+            if square.x == 8:
+                print()
+                print(' '+'-'*39)
+            
 
 class Square:
     def __init__(self,x,y):
@@ -34,18 +45,25 @@ class Square:
         self.connected = []
         self.piece = None
         self.name = 'ABCDEFGH'[x-1] + str(y)
+
+    def printable(self):
+        if self.piece is None:
+            return self.name
+        else:
+            return self.piece.symbol+' '
     
     def __repr__(self):
         connected_names = [sq.name for sq in self.connected]
         return f'Square: {self.name}, piece: {self.piece}, connected: {connected_names}'
 
 class Piece:
-    def __init__(self,type, color):
+    def __init__(self,type, color, symbol):
         self.type = type
         self.color = color
+        self.symbol = symbol
     
     def __repr__(self):
-        return self.type
+        return f'{self.symbol}'
 
 if __name__ == '__main__':
     board = Board()

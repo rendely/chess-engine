6a35154700007d7dab3b4d2c72f7952c68682993
matthew rendely <matthew.rendely@gmail.com>
2025-01-07 20:18:38 -0800

    Add all pieces

--- chess.py
+++ chess.py
@@ -16,14 +16,44 @@ class Board:
                     curr_square.connected.append(square)
 
     def place_pieces(self):
+        # https://www.namecheap.com/visual/font-generator/chess-symbols/
         for square in self.squares:
             #white pawns
             if square.y == 2:
                 square.piece = Piece('Pawn', 'w', '♙')
-
             #black pawns
             if square.y == 7:
                 square.piece = Piece('Pawn', 'b', '♟')
+            #white rooks
+            if square.y == 1 and (square.x == 1 or square.x == 8):
+                square.piece = Piece('Rook','w','♖')
+            #black rooks
+            if square.y == 8 and (square.x == 1 or square.x == 8):
+                square.piece = Piece('Rook','b','♜')
+            #white knights
+            if square.y == 1 and (square.x == 2 or square.x == 7):
+                square.piece = Piece('Knight','w','♘')
+            #black knights
+            if square.y == 8 and (square.x == 2 or square.x == 7):
+                square.piece = Piece('Knight','b','♞')
+            #white bishops
+            if square.y == 1 and (square.x == 3 or square.x == 6):
+                square.piece = Piece('Bishop','w','♗')
+            #black bishops
+            if square.y == 8 and (square.x == 3 or square.x == 6):
+                square.piece = Piece('Bishop','b','♝')
+            #white queen
+            if square.y == 1 and square.x == 4:
+                square.piece = Piece('Queen','w','♕')
+            #black queen
+            if square.y == 8 and square.x == 4:
+                square.piece = Piece('Queen','b','♛')
+            #white king 
+            if square.y == 1 and square.x == 5:
+                square.piece = Piece('King','w','♔')
+            #black king
+            if square.y == 8 and square.x == 5:
+                square.piece = Piece('King','b','♚')
     
         #w pawns
 
